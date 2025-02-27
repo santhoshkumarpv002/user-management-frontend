93ed3a9fa5a743fa56489b889fa870e3b0dd284d
santhoshkumarpv002 <santhoshkumarpv002@gmail.com>
2025-02-27 18:16:32 +0530

    Update api.service.ts

--- src/app/service/api/api.service.ts
+++ src/app/service/api/api.service.ts
@@ -10,7 +10,7 @@ import { User } from '../../dashboard/user.model';
   providedIn: 'root',
 })
 export class ApiService {
-  BASE_URL: string = 'https://user-management-backend-367525621532.asia-south1.run.app';
+  BASE_URL: string = 'https://user-management-backend-test.up.railway.app';
 
   private headers = new HttpHeaders({
     // Authorization: `Bearer ${sessionStorage.getItem('TOKEN')}`,
